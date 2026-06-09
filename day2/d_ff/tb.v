70954d05f7552ca2b3ba134772537c223d488b71
adyakmurali <adyakmurali@gmail.com>
2026-06-09 21:50:58 +0530

    Update tb.v

--- day2/d_ff/tb.v
+++ day2/d_ff/tb.v
@@ -1,1 +1,26 @@
 
+module d_ff_tb(
+
+    );
+   reg d_tb, clk_tb;
+wire q_tb, qbar_tb;
+
+d_ff dut(d_tb, clk_tb, q_tb, qbar_tb);
+
+initial
+begin
+    clk_tb = 0;
+end
+
+always #5 clk_tb = ~clk_tb;
+
+initial
+begin
+    #0  d_tb = 0;
+    #10 d_tb = 1;
+    #10 d_tb = 0;
+    #10 d_tb = 1;
+    #10;
+    end 
+    
+endmodule
