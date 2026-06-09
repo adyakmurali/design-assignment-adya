443d11a06b5d690c814be451fa579375c759b961
adyakmurali <adyakmurali@gmail.com>
2026-06-09 21:47:17 +0530

    Update top.v

--- day2/encoder/design/top.v
+++ day2/encoder/design/top.v
@@ -1,1 +1,16 @@
 
+module encoder(input[3:0]d,output reg [1:0]b
+
+    );
+    always @(*)
+    begin
+    if(d[0])
+    b=2'b00;
+    else if(d[1])
+    b=2'b01;
+    else if(d[2])
+    b=2'b10;
+    else
+    b=2'b11;
+    end
+endmodule
