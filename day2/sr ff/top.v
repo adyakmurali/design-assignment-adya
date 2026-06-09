dbdb0ba1ef8e4110e3dc4bf25e1f5da8fa00ec10
adyakmurali <adyakmurali@gmail.com>
2026-06-09 21:56:12 +0530

    Update top.v

--- day2/sr ff/top.v
+++ day2/sr ff/top.v
@@ -1,1 +1,22 @@
 
+module sr_ff(input s,r,clk,output reg q, qbar
+    );
+    always @(posedge clk)
+begin
+     if(s==0 && r==0)
+        q <= q;
+
+    else if(s==0 && r==1)
+        q <= 0;
+
+    else if(s==1 && r==0)
+        q <= 1;
+
+    else
+        q <= 1'bx;
+end
+always @(*)
+begin
+    qbar = ~q;
+end
+endmodule
