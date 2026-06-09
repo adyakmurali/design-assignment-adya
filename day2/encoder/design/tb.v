5a8ecfd5656d251a8a536244e3c63a8844ab0f94
adyakmurali <adyakmurali@gmail.com>
2026-06-09 23:52:19 +0530

    Update tb.v

--- day2/encoder/design/tb.v
+++ day2/encoder/design/tb.v
@@ -1,1 +1,23 @@
 
+module encoder_tb(
+
+);
+reg [3:0] d_tb;
+wire [1:0] b_tb;
+
+encoder dut(d_tb,b_tb);
+
+initial
+begin
+    d_tb = 4'b0001;
+     #10;
+    d_tb = 4'b0010; 
+    #10;
+    d_tb = 4'b0100;
+     #10;
+    d_tb = 4'b1000;
+     #10;
+
+    $finish;
+end 
+endmodule
