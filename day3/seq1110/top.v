6ee41f66c88c308145f91ef0055fa28d12a17824
adyakmurali <adyakmurali@gmail.com>
2026-06-10 18:29:40 +0530

    Update top.v

--- day3/seq1110/top.v
+++ day3/seq1110/top.v
@@ -1,1 +1,55 @@
+module seq(input clk,rst,din, output reg detected
 
+    );
+    parameter idle=2'b00;
+    parameter s1=2'b01;
+    parameter s2=2'b10;
+    parameter s3=2'b11;
+    reg [1:0]ps,ns;
+    //present state logic
+    always @(posedge clk)
+    begin
+    if(rst)
+    begin
+    ps<=idle;
+    end
+    else
+    ps<=ns;
+    end
+    //next state logic
+    always @(*)
+    begin
+    case(ps)
+    idle:begin
+    detected=0;
+    if(din==0) begin
+    ns=idle;
+    end
+    else
+    ns=s1;
+    end  
+    s1:begin
+     if(din==0) begin
+    ns=idle;
+     end
+    else
+    ns=s2;
+    end
+    s2:begin
+    if(din==0) begin
+    ns=idle;
+    end
+    else
+    ns=s3;
+    end  
+    s3:begin 
+    if(din==0) begin
+    ns=idle;
+    detected=1;
+    end
+    else
+    ns=s2;
+    end
+    endcase  
+    end
+    endmodule
