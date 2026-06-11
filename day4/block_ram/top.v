0d25006a1b36b97280b14a30e4d0d9f9d8cf01a3
adyakmurali <adyakmurali@gmail.com>
2026-06-11 19:20:11 +0530

    Update top.v

--- day4/block_ram/top.v
+++ day4/block_ram/top.v
@@ -1,1 +1,28 @@
 
+module block_ram(input clk, arstn, wr_enb,
+input [7:0] wr_address, read_address, data_in,
+output reg [7:0] dout
+    );
+reg [7:0] mem[255:0];
+integer i;
+always @(posedge clk or negedge arstn)
+begin
+if(!arstn)
+begin
+for(i=0;i<256;i=i+1)
+mem[i]<=0;
+dout<=0;
+end
+else
+begin
+if(wr_enb==1)
+begin
+mem[wr_address]<=data_in;
+end
+else
+begin
+dout<=mem[read_address];
+end
+end
+end
+endmodule
