136976502daf7d99d58f2c8d1d7d00aeed1b0abb
adyakmurali <adyakmurali@gmail.com>
2026-06-14 22:59:35 +0530

    Update top.v

--- day5/bcd_interface/top.v
+++ day5/bcd_interface/top.v
@@ -1,1 +1,35 @@
+//fulladder
 
+module fulladder(input a,b,c,output sum,carry
+    );
+    wire w1,w2,w3;
+    xor(sum,a,b,c);
+    and(w1,a,b);
+    and(w2,b,c);
+    and(w3,a,c);
+    or(carry,w1,w2,w3);
+endmodule
+
+//ripple carry adder
+
+module rca(input [3:0] a, b,cin,output s0, s1, s2, s3,cout
+);
+
+wire c1, c2, c3;
+
+fulladder fa1(a[0], b[0], cin, s0, c1);
+fulladder fa2(a[1], b[1], c1, s1, c2);
+fulladder fa3(a[2], b[2], c2, s2, c3);
+fulladder fa4(a[3], b[3], c3, s3, cout);
+
+endmodule
+
+//bcd adder
+
+module bcd_adder(input [3:0]a,input[3:0]b, input cin,output s0,s1,s2,s3,cout
+);
+wire w0,w1,w2,w3,c,k;
+rca r1(.a(a),.b(b),.cin(cin),.s0(w0),.s1(w1),.s2(w2),.s3(w3),.cout(c));
+assign k = c | (w3 & w2) | (w3 & w1);
+rca r2(.a({w3,w2,w1,w0}),.b({1'b0,k,k,1'b0}),.cin(1'b0),.s0(s0),.s1(s1),.s2(s2),.s3(s3),.cout(cout)); 
+endmodule
