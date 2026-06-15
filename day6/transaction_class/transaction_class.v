8969c50f2eadbfd5163aa4003d73977cc9c60b28
adyakmurali <adyakmurali@gmail.com>
2026-06-15 21:35:19 +0530

    Update transaction_class.v

--- day6/transaction_class/transaction_class.v
+++ day6/transaction_class/transaction_class.v
@@ -1,1 +1,16 @@
+class transaction;
+rand bit rst;
+rand bit wrenb;
+rand bit rdenb;
+rand bit [7:0] data_in;
+bit [7:0] data_out;
+bit full;
+bit empty;
 
+  constraint c1 { wrenb dist {0:=3, 1:=7};}
+
+function void display(string s);
+
+  $display("[%s] rst=%0b wrenb=%0b rdenb=%0b data_in=%0h data_out=%0h full=%0b empty=%0b",s,rst,wrenb,rdenb,data_in,data_out,full,empty);
+endfunction
+endclass
